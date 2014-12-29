9e6c051f8574cf1abd20f8da4cf02cd6549a34d0
Jhonvidal <jhon@gmail.com>
2014-12-29 17:48:24 -0400

    Prueba 1er paso, cargar imagen desde galeria y camara.

--- app/src/main/java/com/jhonvidal/camaraexample/MainActivity.java
+++ app/src/main/java/com/jhonvidal/camaraexample/MainActivity.java
@@ -1,17 +1,162 @@
 package com.jhonvidal.camaraexample;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.jhonvidal.camaraexample.util.PhotoUtils;
+
+import java.io.File;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private AlertDialog _photoDialog;
+    private Uri mImageUri;
+    private static final int ACTIVITY_SELECT_IMAGE = 1020,
+            ACTIVITY_SELECT_FROM_CAMERA = 1040, ACTIVITY_SHARE = 1030;
+    private PhotoUtils photoUtils;
+    private Button photoButton;
+    private ImageView photoViewer;
+    private Boolean fromShare;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        photoButton = (Button) findViewById(R.id.photoButton);
+        photoViewer = (ImageView) findViewById(R.id.photoViewer);
+        fromShare = false;
+
+        photoUtils = new PhotoUtils(this);
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                fromShare = true;
+            } else if (type.startsWith("image/")) {
+                fromShare = true;
+                mImageUri = (Uri) intent
+                        .getParcelableExtra(Intent.EXTRA_STREAM);
+                getImage(mImageUri);
+            }
+        }
+
+        setPhotoButton();
+    }
+
+    private void setPhotoButton() {
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (!getPhotoDialog().isShowing() && !isFinishing())
+                    getPhotoDialog().show();
+            }
+        });
+    }
+
+
+    private AlertDialog getPhotoDialog() {
+        if (_photoDialog == null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    this);
+            builder.setTitle(R.string.photo_source);
+            builder.setPositiveButton(R.string.camera, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(
+                            "android.media.action.IMAGE_CAPTURE");
+                    File photo = null;
+                    try {
+                        // place where to store camera taken picture
+                        photo = PhotoUtils.createTemporaryFile("picture", ".jpg", MainActivity.this);
+                        photo.delete();
+                    } catch (Exception e) {
+                        Log.v(getClass().getSimpleName(),
+                                "Can't create file to take picture!");
+                    }
+
+                    mImageUri = Uri.fromFile(photo);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+                    startActivityForResult(intent, ACTIVITY_SELECT_FROM_CAMERA);
+
+                }
+
+            });
+            builder.setNegativeButton(R.string.gallery, new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    galleryIntent.setType("image/*");
+                    startActivityForResult(galleryIntent, ACTIVITY_SELECT_IMAGE);
+                }
+
+            });
+            _photoDialog = builder.create();
+
+        }
+        return _photoDialog;
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mImageUri != null)
+            outState.putString("Uri", mImageUri.toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.containsKey("Uri")) {
+            mImageUri = Uri.parse(savedInstanceState.getString("Uri"));
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTIVITY_SELECT_IMAGE && resultCode == RESULT_OK) {
+            mImageUri = data.getData();
+            getImage(mImageUri);
+        } else if (requestCode == ACTIVITY_SELECT_FROM_CAMERA
+                && resultCode == RESULT_OK) {
+            getImage(mImageUri);
+        }
+    }
+
+    public void getImage(Uri uri) {
+        Bitmap bounds = photoUtils.getImage(uri);
+        if (bounds != null) {
+            setImageBitmap(bounds);
+        } else {
+            showErrorToast();
+        }
+    }
+
+    private void showErrorToast() {
+       Log.e("ERROR","ERROR AL CARGAR");
+    }
+
+    private void setImageBitmap(Bitmap bitmap){
+        photoViewer.setImageBitmap(bitmap);
     }
 
 
@@ -36,4 +181,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/jhonvidal/camaraexample/model/PictureData.java
@@ -0,0 +1,14 @@
+package com.jhonvidal.camaraexample.model;
+
+import android.widget.ImageView;
+
+import java.net.URI;
+
+/**
+ * Created by jhonvidal on 29-12-14.
+ */
+public class PictureData {
+    public ImageView imageView;
+    public Boolean requerido;
+    public URI uri;
+}
